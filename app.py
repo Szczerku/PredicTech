d5e814dc972d65400d2ba2452ca1847473215ff0
Szczerku <93645221+Szczerku@users.noreply.github.com>
2023-11-13 21:07:57 +0100

    improved js file

--- app.py
+++ app.py
@@ -1,17 +1,56 @@
-from flask import Flask, render_template
 import random
+from threading import Lock
+from flask_socketio import SocketIO
+from datetime import datetime
+from flask import Flask, render_template, request
+import mysql.connector
 
+thread_lock = Lock()
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'szczery!'
+socketio = SocketIO(app, cors_allowed_origins='*')
 
-@app.route("/")
-def home():
-    labels1 = [i + 1 for i in range(50)]
-    values1 = [random.randint(1, 20) for _ in range(50)]
+def fetch_data_from_database():
+    connection = mysql.connector.connect(
+        host='192.168.1.15',
+        user='szczery',
+        password='Allegro123@',
+        database='twoja_nowa_baza'
+    )
 
-    labels2 = [i + 1 for i in range(50)]
-    values2 = [random.randint(1, 20) for _ in range(50)]
+    cursor = connection.cursor()
+    cursor.execute('SELECT random_number FROM your_table ORDER BY id DESC LIMIT 1')
+    data = cursor.fetchall()
+    cursor.close()
+    connection.close()
 
-    labels3 = [i + 1 for i in range(50)]
-    values3 = [random.randint(1, 20) for _ in range(50)]
+    return [item[0] for item in data] if data else None
 
-    return render_template('home.html', labels1=labels1, values1=values1, labels2=labels2, values2=values2, labels3=labels3, values3=values3)
+def get_current_datetime():
+    now = datetime.now()
+    return now.strftime("%m/%d/%Y %H:%M:%S")
+
+def background_thread(chart_id):
+    print(f"Generowanie losowych wartości dla wykresu {chart_id}")
+    while True:
+        dummy_sensor_value = fetch_data_from_database()[0]
+        socketio.emit(f'updateSensorData{chart_id}', {'value': dummy_sensor_value, "date": get_current_datetime()})
+        socketio.sleep(1)
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+@socketio.on('connect')
+def connect():
+    print('Klient połączony')
+    with thread_lock:
+        for i in range(1, 4): 
+            socketio.start_background_task(background_thread, i)
+
+@socketio.on('disconnect')
+def disconnect():
+    print('Klient rozłączony',  request.sid)
+
+if __name__ == '__main__':
+    socketio.run(app)
---
+++ static/js/app.js
@@ -0,0 +1,86 @@
+$(document).ready(function () {
+    const MAX_DATA_COUNT = 50;
+
+    function configureChart(ctx, label, backgroundColor, borderColor) {
+        return new Chart(ctx, {
+            type: "line",
+            data: {
+                labels: [],
+                datasets: [
+                    {
+                        label: label,
+                        fill: true,
+                        backgroundColor: backgroundColor,
+                        borderColor: borderColor,
+                        lineTension: 0.35,
+                        data: []
+                    }
+                ]
+            },
+            options: {
+                scales: {
+                    x: {
+                        grid: {
+                            display: false
+                        }
+                    },
+                    y: {
+                        grid: {
+                            display: false
+                        }
+                    }
+                },
+                responsive: true,
+                maintainAspectRatio: false,
+            },
+        });
+    }
+
+    function addData(lineChart, label, data) {
+        lineChart.data.labels.push(label);
+        lineChart.data.datasets.forEach((dataset) => {
+            dataset.data.push(data);
+        });
+        lineChart.update();
+    }
+
+    function removeFirstData(lineChart) {
+        lineChart.data.labels.splice(0, 1);
+        lineChart.data.datasets.forEach((dataset) => {
+            dataset.data.shift();
+        });
+        lineChart.update();
+    }
+
+    function handleSocketData(lineChart, msg) {
+        console.log(`Otrzymano dane z czujnika :: ${msg.date} :: ${msg.value}`);
+
+        if (lineChart.data.labels.length > MAX_DATA_COUNT) {
+            removeFirstData(lineChart);
+        }
+        addData(lineChart, msg.date, msg.value);
+    }
+
+    const ctx1 = document.getElementById("lineChart1").getContext("2d");
+    const lineChart1 = configureChart(ctx1, "Voltage 1", "rgba(75,192,192,0.5)", "rgb(75, 192, 192)");
+
+    const ctx2 = document.getElementById("lineChart2").getContext("2d");
+    const lineChart2 = configureChart(ctx2, "Voltage 2", "rgba(255, 0, 255, 0.5)", "rgb(255,0 , 255)");
+
+    const ctx3 = document.getElementById("lineChart3").getContext("2d");
+    const lineChart3 = configureChart(ctx3, "Voltage 3", "rgba(31,53,235,0.5)", "rgb(31, 53, 235)");
+
+    var socket = io.connect("http://127.0.0.1:5000/");
+
+    socket.on("updateSensorData1", function (msg) {
+        handleSocketData(lineChart1, msg);
+    });
+
+    socket.on("updateSensorData2", function (msg) {
+        handleSocketData(lineChart2, msg);
+    });
+
+    socket.on("updateSensorData3", function (msg) {
+        handleSocketData(lineChart3, msg);
+    });
+});
